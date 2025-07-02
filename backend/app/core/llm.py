1b686b98e0a66c802d73753ee541930c5c9c88ac
phanlerda <phanlerda@gmail.com>
2025-07-02 21:13:07 +0700

    update model gemini 2.5 pro

--- backend/app/core/llm.py
+++ backend/app/core/llm.py
@@ -13,7 +13,7 @@ def get_llm():
     """
     try:
         model = ChatGoogleGenerativeAI(
-            model="gemini-1.5-flash-latest",
+            model="gemini-2.5-flash",
             temperature=0.5,
             top_p=1,
             top_k=1,
